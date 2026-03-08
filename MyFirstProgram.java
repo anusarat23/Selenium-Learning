3e95bc72069de12140dbcb2a3f25c6c025b07cfc
anusarat23 <anuvyjayanthi@gmail.com>
2026-03-08 18:45:46 +0530

    Add files via upload
    day1

---
+++ MyFirstProgram.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class MyFirstProgram {
+    public static void main (String[] args)
+    {
+        System.out.println();
+        System.out.println("Hello");
+        System.out.println("World"); 
+    } 
+
+}
